e583c766a2b45dd633318389d92a8e8c4e96d38a
douglastenfen <douglastenfen@gmail.com>
2022-10-11 20:54:06 -0300

    dotenv added

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv/config";
 
 mongoose.connect(
-  "mongodb+srv://douglastenfen:romaGDs2@cluster0.l8eriiq.mongodb.net/alura-node?"
+  `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.l8eriiq.mongodb.net/alura-node?`
 );
 
 let db = mongoose.connection;
